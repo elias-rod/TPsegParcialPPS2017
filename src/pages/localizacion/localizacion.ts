9f554cf197bc73463f4dd7ac0699e2c8e4ba5c4d
virtualgithub <elias.rod@live.com>
2017-08-02 18:07:20 -0300

    mapas funcionando correctamente

--- src/pages/localizacion/localizacion.ts
+++ src/pages/localizacion/localizacion.ts
@@ -20,7 +20,13 @@ import {
   templateUrl: 'localizacion.html'
 })
 
+
 export class Localizacion {
+  
+  mostrar;
+  spinner;
+  map : GoogleMap;
+
   constructor(public navCtrl: NavController,
   public toastCtrl: ToastController,
   private nativeAudio: NativeAudio,
@@ -30,43 +36,56 @@ export class Localizacion {
   private googleMaps: GoogleMaps) {
     this.nativeAudio.preloadSimple('yay', 'assets/sounds/yay.wav');
     this.nativeAudio.preloadSimple('error', 'assets/sounds/error.mp3');
-  }
-
-  ElegirEspanol(){
-    this.translate.setDefaultLang('es');
-    this.vibration.vibrate(100);
-    this.nativeAudio.play('yay');
-  }
-  // Load map only after view is initialized
-  ngAfterViewInit() {
-    this.loadMap();
-  }
-
-  loadMap() {
-    // create a new map by passing HTMLElement
-    let element: HTMLElement = document.getElementById('map');
-
-    let map: GoogleMap = this.googleMaps.create(element);
-
-    let latLng = new LatLng(-34.599722, -58.381944);
-
-    map.one(GoogleMapsEvent.MAP_READY)
-    .then(()=>{
-      let position: CameraPosition = {
-        target: latLng,
-        zoom: 3,
-        tilt: 0
+    this.mostrar = false;
+    translate.get('Cargando...').subscribe(
+      translatedText => {
+        this.spinner = this.loadingCtrl.create({
+          content: translatedText
+        });
       }
-      map.moveCamera(position);
-      
-      let markerOptions: MarkerOptions = {
-        position: latLng
-      };
+    );
+  }
 
-      let marker = map.addMarker(markerOptions)
-      .then((marker: Marker) => {
-            marker.showInfoWindow();
-      });
+  DesecharYCrearSpinner(){
+    this.spinner.dismiss();
+    this.translate.get('Cargando...').subscribe(
+      translatedText => {
+        this.spinner = this.loadingCtrl.create({
+          content: translatedText
+        });
+      }
+    );
+  }
+
+  ngAfterViewInit() {
+    let element: HTMLElement = document.getElementById('map');
+    this.map = this.googleMaps.create(element);
+    this.spinner.present();
+    this.map.one(GoogleMapsEvent.MAP_READY)
+    .then(()=>{
+      this.mostrar = true;
+      this.DesecharYCrearSpinner();
+    });
+  }
+
+  ubicarPosicion(lat, lng){
+    let latLng = new LatLng(lat, lng);
+    this.map.clear();
+    let position: CameraPosition = {
+      target: latLng,
+      zoom: 3,
+      tilt: 0
+    }
+    this.map.moveCamera(position);
+    let markerOptions: MarkerOptions = {
+      position: latLng
+    };
+
+    this.spinner.present();
+    let marker = this.map.addMarker(markerOptions)
+    .then((marker: Marker) => {
+      marker.showInfoWindow();
+      this.DesecharYCrearSpinner();
     });
   }
 }
